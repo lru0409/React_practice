aa01352cd9e1f1e8e4753e0926a776d3aebfd04b
이로운 <lru040409@gmail.com>
2024-09-02 18:52:46 +0900

    chapter13

---
+++ src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+  return (
+    <Card title="Rowoon Lee" backgroundColor="skyblue">
+      <p>안녕하세요. 이로운입니다.</p>
+      <p>저는 리액트를 사용해서 개발하고 있습니다.</p>
+    </Card>
+  );
+}
+
+export default ProfileCard;
