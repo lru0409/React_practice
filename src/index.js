8d78bcd6c9719ad059a429f8108abeed53474914
이로운 <lru040409@gmail.com>
2024-09-02 18:26:25 +0900

    chapter12

--- src/index.js
+++ src/index.js
@@ -13,11 +13,12 @@ import ConfirmButton from "./chapter_08/ConfirmButton";
 import LandingPage from "./chapter_09/LandingPage";
 import AttendanceBook from "./chapter_10/AttendanceBook";
 import Signup from "./chapter_11/Singup";
+import Calculator from "./chapter_12/Calculator";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <Signup />
+  <Calculator />
   // </React.StrictMode>
 );
 
