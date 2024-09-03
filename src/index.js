2c7b4dfbe4285fe1d9bdf33110656c04459af262
이로운 <lru040409@gmail.com>
2024-09-03 16:48:53 +0900

    mini_blog

--- src/index.js
+++ src/index.js
@@ -20,9 +20,9 @@ import Blocks from "./chapter_15/Blocks";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <Blocks />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
