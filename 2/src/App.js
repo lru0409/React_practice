b2393525825c5bcda90287a9c3799dcf79ea701d
이로운 <lru040409@gmail.com>
2024-09-11 15:45:10 +0900

    chapter9 sass

--- 2/src/App.js
+++ 2/src/App.js
@@ -5,9 +5,10 @@ import LifecycleSampleApp from "./chapter_07/LifeCycleSample";
 import Info from "./chapter_08/info";
 import Counter from "./chapter_08/Counter";
 import Average from "./chapter_08/Average";
+import SassComponent from "./chapter_09/SassComponent";
 
 function App() {
-  return <Average />;
+  return <SassComponent />;
 }
 
 export default App;
