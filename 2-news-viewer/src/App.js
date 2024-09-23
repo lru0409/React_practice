9af8154734152082101e5ca89dfacfa541281c65
이로운 <lru040409@gmail.com>
2024-09-23 16:31:50 +0900

    chapter 14, React Router 적용

---
+++ 2-news-viewer/src/App.js
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-route-dom';
+import NewsPage from './pages/NewsPage';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<NewsPage />} />
+      <Route path="/:category" element={<NewsPage />} />
+    </Routes>
+  );
+};
+
+export default App;
