d93038f6bcb03a059d7ace701d24a3620884ab28
이로운 <lru040409@gmail.com>
2024-10-06 15:56:33 +0900

    chapter 20 before eject

---
+++ 2-server-side-rendering/src/App.js
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Menu from "./components/Menu";
+import RedPage from "./pages/RedPage";
+import BluePage from "./pages/BluePage";
+
+const App = () => {
+  return (
+    <div>
+      <Menu />
+      <hr />
+      <Routes>
+        <Route path="/red" element={<RedPage />} />
+        <Route path="/blue" element={<BluePage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
