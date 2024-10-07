8b07ea8fda5ddcf3f3e587d7c35de834b6597448
이로운 <lru040409@gmail.com>
2024-10-07 16:32:54 +0900

    chatper 20 ing

--- 2-server-side-rendering/src/index.js
+++ 2-server-side-rendering/src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { thunk } from "redux-thunk";
+import rootReducer from "./modules";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- 2-server-side-rendering/src/index.server.js
+++ 2-server-side-rendering/src/index.server.js
@@ -1,7 +1,86 @@
 import ReactDOMServer from "react-dom/server";
+import express from "express";
+import { StaticRouter } from "react-router-dom/server";
+import App from "./App";
+import path from "path";
+import fs from "fs";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { thunk } from "redux-thunk";
+import rootReducer from "./modules";
+import PreloadContext from "./lib/PreloadContext";
 
-const html = ReactDOMServer.renderToString(
-  <div>Hello Server Side Rendering!</div>
+// asset-manifest.json에서 파일 경로를 조회
+const manifest = JSON.parse(
+  fs.readFileSync(path.resolve("./build/asset-manifest.json"), "utf8")
 );
 
-console.log(html);
+const chunks = Object.keys(manifest)
+  .filter((key) => /chunk\.js$/.exec(key)) // chunk.js로 끝나는 키를 찾아서
+  .map((key) => `<script src="${manifest.files[key]}"></script>`) // 스크립트 태그로 변환하고
+  .join(" "); // 합침
+
+function createPage(root) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="there-color" content="#000000" />
+    <title>React App</title>
+    <link href="${manifest.files["main.css"]}" rel="stylesheet" />
+  </head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root">${root}</div>
+    <script src="${manifest.files["runtime-main.js"]}"></scripts>
+    ${chunks}
+    <script src="${manifest.files["main.js"]}"></scripts>
+  </body>
+  </html>
+  `;
+}
+
+const app = express();
+
+// 서버 사이드 렌더링을 처리할 핸들러 함수
+const serverRender = async (req, res, next) => {
+  // 404가 떠야 하는 상황에서 404를 띄우지 않고 서버 사이드 렌더링을 해줌
+  const context = {};
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+  });
+  const preloadContext = { done: false, promises: [] };
+  const jsx = (
+    <PreloadContext.Provider value={preloadContext}>
+      <Provider store={store}>
+        <StaticRouter location={req.url} context={context}>
+          <App />
+        </StaticRouter>
+      </Provider>
+    </PreloadContext.Provider>
+  );
+
+  ReactDOMServer.renderToStaticMarkup(jsx); // renderToStaticMarkup으로 한 번 렌더링함
+  try {
+    await Promise.all(preloadContext.promises); // 모든 프로미스를 기다림
+  } catch (e) {
+    return res.status(500);
+  }
+  preloadContext.done = true;
+  const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
+  res.send(createPage(root)); // 클라이언트에게 결과물을 응답함
+};
+
+const serve = express.static(path.resolve("./build"), {
+  index: false,
+}); // "/" 경로에서 index.html을 보여주지 안도록 설정
+
+app.use(serve); // 순서가 중요함. serverRender 전에 위치해야 함.
+app.use(serverRender);
+app.listen(5001, () => {
+  // 5001 포트로 서버 가동
+  console.log("Running on http://localhost:5001");
+});
