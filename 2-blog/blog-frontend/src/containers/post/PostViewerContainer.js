fe6a3665671d1867d598abd7fe04724b63cf783f
lru0409 <lru040409@gmail.com>
2025-09-09 19:13:15 +0900

    chapter 26 implement post reading

---
+++ 2-blog/blog-frontend/src/containers/post/PostViewerContainer.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { readPost, unloadPost } from '../../modules/post';
+import PostViewer from '../../components/post/PostViewer';
+
+const PostViewerContainer = () => {
+  const { postId } = useParams();
+  const dispatch = useDispatch();
+  const { post, error, loading } = useSelector(({ post, loading }) => ({
+    post: post.post,
+    error: post.error,
+    loading: loading.GET_POST,
+  }));
+
+  useEffect(() => {
+    dispatch(readPost(postId));
+    // 언마운트될 때 리덕스에서 포스트 데이터 없애기
+    return () => {
+      dispatch(unloadPost());
+    };
+  }, [dispatch, postId]);
+
+  return <PostViewer post={post} loading={loading} error={error} />;
+};
+
+export default PostViewerContainer;
